0fff621daf540dfeef0c06e2754132021e582964
ranjt12103 <ranjitppatil10@gmail.com>
2026-01-01 22:03:30 +0530

    CRUD Functionality fot manage User

--- dashboards/views.py
+++ dashboards/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
 
-from dashboards.forms import BlogPostForm, CategoryForm
+from dashboards.forms import AddUserForm, BlogPostForm, CategoryForm, EditUserForm
 from django.template.defaultfilters import slugify
 
 from django.contrib.auth.models import User
@@ -120,6 +120,40 @@ def users(request):
         'users': users,
     }
     return render(request, 'dashboard/users.html', context)
+
+def add_user(request):
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    form = AddUserForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/add_user.html', context)
+
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = EditUserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+    form = EditUserForm(instance=user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/edit_user.html', context)
+
+
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
     
     
     
